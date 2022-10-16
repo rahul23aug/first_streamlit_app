7d622f2782d9e661f6c67d88a30593872f2915d8
rahul23aug <67165234+rahul23aug@users.noreply.github.com>
2022-10-16 23:35:04 +0530

    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit as st
-st.title("<H1> MY FIRST STREAMLIT APP")
+st.title("MY FIRST STREAMLIT APP")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

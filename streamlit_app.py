8be1ecd56598233eb43a68d5f373e6c2d2db616d
rahul23aug <67165234+rahul23aug@users.noreply.github.com>
2022-11-12 21:52:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
+#streamlit.text(fruityvice_response)
+#normalize json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# get df
+streamlit.dataframe(fruityvice_normalized)
